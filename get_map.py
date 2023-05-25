c8892d6ea6c88c74d50d1b45f72c76e931e0473c
voidbean <41186092+voidbean@users.noreply.github.com>
2023-05-25 11:14:30 +0800

    Update get_map.py
    add ranked map

--- get_map.py
+++ get_map.py
@@ -19,11 +19,17 @@ async def get_map(bot, ev: CQEvent, args):
     br_map_en = rep_json["battle_royale"]["current"]["map"].lower()  # 大逃杀的名字英文
     br_map_zh = map_name_dict["br"].get(br_map_en, br_map_en)  # 大逃杀的名字中文
     br_remaining = rep_json["battle_royale"]["current"]["remainingMins"]  # 大逃杀剩余时间
+    
+    ranked_map_en = rep_json["ranked"]["current"]["map"].lower()  # 排位的名字英文
+    ranked_map_zh = map_name_dict["br"].get(br_map_en, br_map_en)  # 排位的名字中文
+    ranked_remaining = rep_json["ranked"]["current"]["remainingMins"]  # 排位剩余时间
 
     content = f"""
 当前地图
 > 大逃杀: {br_map_zh}
 \u23F0剩余时间: {int(br_remaining)}分钟
+> 排位: {ranked_map_zh}
+\u23F0剩余时间: {int(ranked_remaining)}分钟
 > 竞技场: {arenas_map_zh}
 \u23F0剩余时间: {int(arenas_remaining)}分钟
 """.strip()
@@ -39,10 +45,16 @@ async def get_future_map(bot, ev: CQEvent, args):
     br_map_en = rep_json["battle_royale"]["next"]["map"].lower()  # 大逃杀的名字英文
     br_map_zh = map_name_dict["br"].get(br_map_en, br_map_en)  # 大逃杀的名字中文
     br_start_time = recalculate_timezone(rep_json["battle_royale"]["next"]["readableDate_start"])
+    
+    ranked_map_en = rep_json["ranked"]["current"]["map"].lower()  # 排位的名字英文
+    ranked_map_zh = map_name_dict["br"].get(br_map_en, br_map_en)  # 排位的名字中文
+    ranked_start_time = recalculate_timezone(rep_json["ranked"]["next"]["readableDate_start"])  # 排位剩余时间
 
     content = f"""
 地图轮换| 下一张地图是
 > 大逃杀: {br_map_zh}
+开始时间: {br_start_time}
+> 排位: {ranked_map_zh}
 开始时间: {br_start_time}
 > 竞技场: {arenas_map_zh}
 开始时间: {arenas_start_time}
